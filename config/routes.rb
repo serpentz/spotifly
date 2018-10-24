3b9a9776d07e9716d33a45711cf93145ee4137ab
pasolre95kim <pasolre95kim@gmail.com>
2018-10-24 11:50:24 -0400

    edited login & register page

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
- 
+
   resources :songs, only: [:show, :delete, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  post "users/index", to: 'users#check_creds', as: 'check_creds'
 # get "users/playlist/new", to: 'users#create_playlist', as: 'create_playlist'
 # get "users/register", to: 'users#register', as: 'register'
-# post "users/register", to: 'users#create'
+post "users/new", to: 'users#create'
 
 resources :users
 resources :playlists, only: [:new, :create]
 
 
- 
+
 
   root "users#index"
 end
