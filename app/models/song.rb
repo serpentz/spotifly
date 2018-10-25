28cdb57eb1fa1c537d7c06687bdd00b681f79543
pasolre95kim <pasolre95kim@gmail.com>
2018-10-25 09:49:09 -0400

    fixed song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,7 @@
 class Song < ApplicationRecord
-	belongs_to :playlist, optional: true
-<<<<<<< HEAD
-	has_many :user_votes
-	has_many :votes, through: :user_votes
-	validates :title, uniqueness: true
-=======
-	
+
 	has_many :votes
->>>>>>> c69e5de15a1915d34ead869e127915b2341bc552
+	belongs_to :playlist, optional: true
+	validates :title, uniqueness: true
+
 end
